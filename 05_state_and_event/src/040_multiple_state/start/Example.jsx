d533502e5545cabd5d9212ba0f1dc8444d40541b
hasedai0000 <hasegawa.daichi.workman@gmail.com>
2024-04-25 01:16:54 +0900

    useStateの複数使用時の注意点をインプット

--- 05_state_and_event/src/040_multiple_state/start/Example.jsx
+++ 05_state_and_event/src/040_multiple_state/start/Example.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+
 const Example = () => {
-  
+  let [valA, setValA] = useState(0);
+  let [valB, setValB] = useState(0);
+  let [valC, setValC] = useState(0);
+  const onClickButtonA = () => {
+    setValA(valA + 1);
+  };
+  const onClickButtonB = () => {
+    setValB(valB + 1);
+  };
+  const onClickButtonC = () => {
+    setValC(valC + 1);
+  };
+
+  return (
+    <>
+      <p>ボタンAを{valA}回押しました!</p>
+      <button onClick={onClickButtonA}>ボタンA</button>
+      <p>ボタンBを{valB}回押しました!</p>
+      <button onClick={onClickButtonB}>ボタンB</button>
+      <p>ボタンCを{valC}回押しました!</p>
+      <button onClick={onClickButtonC}>ボタンC</button>
+    </>
+  );
 };
 
 export default Example;
