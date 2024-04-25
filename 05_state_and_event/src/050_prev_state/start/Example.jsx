923c23328afba4eb455594ca25d2b673506bacac
hasedai0000 <hasegawa.daichi.workman@gmail.com>
2024-04-25 11:06:33 +0900

    useStateの使い方の基礎

--- 05_state_and_event/src/050_prev_state/start/Example.jsx
+++ 05_state_and_event/src/050_prev_state/start/Example.jsx
@@ -1,7 +1,26 @@
 import { useState } from "react";
 
 const Example = () => {
-  
+  let [count, setCount] = useState(0);
+
+  const countUp = () => {
+    setCount(count + 1);
+    // 現在のステートの値を使用する場合は以下のようにする
+    setCount((prevstate) => {
+      return prevstate + 1;
+    });
+  };
+
+  const countDown = () => {
+    setCount(count - 1);
+  };
+  return (
+    <>
+      <p>現在のカウント数:{count}</p>
+      <button onClick={countUp}>+</button>
+      <button onClick={countDown}>-</button>
+    </>
+  );
 };
 
 export default Example;
