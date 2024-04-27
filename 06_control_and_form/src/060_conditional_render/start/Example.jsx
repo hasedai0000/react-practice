57f35c8c489bab8d536e55ea27ce02c4b2feef2b
hasedai0000 <hasegawa.daichi.workman@gmail.com>
2024-04-27 17:52:24 +0900

    JSXの条件分岐の記法

--- 06_control_and_form/src/060_conditional_render/start/Example.jsx
+++ 06_control_and_form/src/060_conditional_render/start/Example.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const Example = () => {
-  const animals = ["Dog", "Cat", "Rat"];
+  const animals = ["Dog", "Cat", null, "Rat"];
 
   const [filterVal, setFilterVal] = useState("");
 
@@ -15,13 +15,35 @@ const Example = () => {
       <ul>
         {animals
           .filter((animal) => {
-            const isMatch = animal.indexOf(filterVal) !== -1;
-            console.log(animal.indexOf(filterVal));
+            const animalStr = animal ?? "";
+            const isMatch = animalStr.indexOf(filterVal) !== -1;
+
             return isMatch;
           })
-          .map((animal) => (
-            <li key={animal}>{animal}</li>
-          ))}
+          .map((animal) => {
+            return (
+              <li key={animal}>
+                {
+                  // 通常のIF文
+                  // if (animal === "Dog") {
+                  //   return <li key={animal}>{animal + "★"}</li>;
+                  // } else {
+                  //   return <li key={animal}>{animal}</li>;
+                  // }
+
+                  // 三項演算子
+                  // return (
+                  //   <li key={animal}>{animal + (animal === "Dog" ? "★" : "")}</li>
+                  // );
+
+                  // null合体演算子
+                  animal ?? "null, undefinedでした"
+                }
+                {/* &&演算子 */}
+                {animal === "Dog" && "★"}
+              </li>
+            );
+          })}
       </ul>
     </>
   );
