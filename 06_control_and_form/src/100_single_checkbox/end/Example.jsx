560530b6218e3a118fef3e83a5ccff59b280deaf
hasedai0000 <hasegawa.daichi.workman@gmail.com>
2024-04-28 10:40:41 +0900

    複数のチェックボックスの実装

--- 06_control_and_form/src/100_single_checkbox/end/Example.jsx
+++ 06_control_and_form/src/100_single_checkbox/end/Example.jsx
@@ -1,25 +1,69 @@
 import { useState } from "react";
 
-// POINT チェックボックスの実装
+// end用:startと値が被らないための接続詞
+const prefix = "end-";
+// POINT 複数チェックボックスの実装
 const Example = () => {
-  const [isChecked, setIsChecked] = useState(true);
+  const [fruits, setFruits] = useState([
+    { label: "Apple", value: 100, checked: false },
+    { label: "Banana", value: 200, checked: false },
+    { label: "Cherry", value: 300, checked: false },
+  ]);
 
-  // const toggleChecked = (e) => {
-  //   setIsChecked(prevState => !prevState);
-  // };
+  const [sum, setSum] = useState(0);
 
+  const handleChange = (e) => {
+    const newFruits = fruits.map((fruit) => {
+      const newFruit = { ...fruit };
+      if (newFruit.label === e.target.value) {
+        newFruit.checked = !fruit.checked;
+      }
+
+      return newFruit;
+    });
+
+    setFruits(newFruits);
+    // forEachバージョン
+    // let sumVal = 0;
+    // newFruits.forEach(fruit => {
+    //   if(fruit.checked) {
+    //     sumVal = sumVal + fruit.value;
+    //   }
+    // });
+
+    // filter + forEachバージョン
+    // let sumVal = 0;
+    // newFruits
+    //   .filter((fruit) => fruit.checked)
+    //   .forEach((fruit) => (sumVal = sumVal + fruit.value));
+
+    // filter + reduceバージョン
+    let sumVal = newFruits
+      .filter((fruit) => fruit.checked)
+      .reduce((sumVal, fruit) => sumVal + fruit.value, 0);
+    setSum(sumVal);
+  };
   return (
     <div>
-      <label htmlFor="my-check">
-        チェック：
-      </label>
-      <input
-        type="checkbox"
-        id="my-check"
-        checked={isChecked}
-        onChange={() => setIsChecked(prevState => !prevState)}
-      />
-      <div>{isChecked ? "ON!" : "OFF!"}</div>
+      {fruits.map((fruit) => {
+        return (
+          <div key={fruit.label}>
+            <input
+              // 接続詞追加
+              id={prefix + fruit.label}
+              type="checkbox"
+              value={fruit.label}
+              checked={fruit.checked}
+              onChange={handleChange}
+            />
+            {/* 接続詞追加 */}
+            <label htmlFor={prefix + fruit.label}>
+              {fruit.label}:{fruit.value}
+            </label>
+          </div>
+        );
+      })}
+      <div>合計：{sum}</div>
     </div>
   );
 };
