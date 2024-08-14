72a966fb0f02f79c64d4ebf6ddf59529429091d6
d.hasegawa <d.hasegawa@styleedge.co.jp>
2024-08-14 22:56:14 +0900

    useReducerの練習

--- 11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
+++ 11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
@@ -1,7 +1,40 @@
+import { useReducer } from "react";
+import { useState } from "react";
+
+// useReducerはuseStateの書き換えに使用
 const Example = () => {
-  
+  const [state, setState] = useState(0);
+  const [rstate, dispatch] = useReducer((prev, action) => {
+    if (action === "+") {
+      return ++prev;
+    } else {
+      return --prev;
+    }
+  }, 0);
+
+  const countUp = () => {
+    setState((prev) => ++prev);
+  };
+
+  const rcountUp = () => {
+    dispatch("+");
+  };
+
+  const rcountDown = () => {
+    dispatch("-");
+  };
+
   return (
     <>
+      <div>
+        <h3>{state}</h3>
+        <button onClick={countUp}>+</button>
+      </div>
+      <div>
+        <h3>{rstate}</h3>
+        <button onClick={rcountUp}>+</button>
+        <button onClick={rcountDown}>-</button>
+      </div>
     </>
   );
 };
